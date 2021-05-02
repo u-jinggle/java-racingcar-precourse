148e168ee6a59dc9ae67af099f9961603ce8a6a1
이유진 <ujin@deali.net>
2021-05-02 19:01:38 +0900

    feat: Car List를 전달받아 Cars객체 구성

---
+++ src/main/java/Cars.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
