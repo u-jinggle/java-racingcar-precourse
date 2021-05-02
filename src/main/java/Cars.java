cb7c5ffca642b33a319d75e11f86611354818cb7
이유진 <ujin@deali.net>
2021-05-02 20:27:52 +0900

    feat: 입력받은 시도회수에 만큼 cars 객체들의 play를 실행하고 결과값을 출력한다

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -10,4 +10,19 @@ public class Cars {
     public List<Car> getCars() {
         return cars;
     }
+
+    public void play(int playCount) {
+        for (int count = 0; count < playCount; count++) {
+            playCar();
+            System.out.println();
+        }
+    }
+
+    private void playCar() {
+        for (Car car : cars) {
+            RandomNum randomNum = new RandomNum();
+            car.moveForward(randomNum.getNum());
+            System.out.println(car.playResult());
+        }
+    }
 }
