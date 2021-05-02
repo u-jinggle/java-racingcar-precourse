e110ffceb5d34eb69ef83fb5c0f65f0b4029a194
이유진 <ujin@deali.net>
2021-05-02 20:21:22 +0900

    test: car의 전진 회수에 따라 표기될 출력값이 맞는지 비교 Test

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,3 +1,4 @@
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ class CarTest {
     private Car car;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         carName = "test";
         car = new Car(carName);
     }
@@ -26,15 +27,28 @@ class CarTest {
     void moveForwardTest() {
         int beforeMovePosition = car.getPosition();
         car.moveForward(4);
-        assertThat(car.getPosition()).isEqualTo(beforeMovePosition+1);
+        assertThat(car.getPosition()).isEqualTo(beforeMovePosition + 1);
     }
 
     @Test
     @DisplayName("입력받은 randomInt값이 4보다 작을 경우 position값 그대로 인지 Test")
-    void noMoveForwardTest(){
+    void noMoveForwardTest() {
         int beforeMovePosition = car.getPosition();
         car.moveForward(3);
         assertThat(car.getPosition()).isEqualTo(beforeMovePosition);
+    }
+
+    @Test
+    @DisplayName("car의 moveForward 이후 출력될 문자열이 기대한 값과 같은지 비교 Test")
+    void playResultTest() {
+        int playCount = 4;
+        String bar = "";
+        for (int i = 0; i < playCount; i++) {
+            car.moveForward(9);
+            bar = bar.concat("-");
+        }
+        String expectResult = carName+" : " + bar;
+        AssertionsForClassTypes.assertThat(car.playResult()).isEqualTo(expectResult);
     }
 
 }
