58b7a3b3a3104f3ff2e63ce1ae5a0efb293c700b
이유진 <ujin@deali.net>
2021-05-02 18:19:16 +0900

    feat: 0부터 9 사이의 랜덤 int 생성

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,11 +1,26 @@
+import java.util.Random;
+
 public class Util
 {
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+    private static Random random = new Random();
+
     private Util(){}
-    private static String failValidStringLength = "자동차 이름은 5글자 이하로 설정 가능합니다.";
+
+    private static final String FAIL_VALID_STRING_LENGTH = "자동차 이름은 5글자 이하로 설정 가능합니다.";
 
     public static void checkValidStringLength(String carName){
         if(carName.length() > 5){
-            throw new IllegalArgumentException(failValidStringLength);
+            throw new IllegalArgumentException(FAIL_VALID_STRING_LENGTH);
         }
+    }
+
+    public static String[] stringToArray(String inputCars) {
+        return inputCars.split(",");
+    }
+
+    public static Integer createRandomInt() {
+        return random.nextInt(RANDOM_MIN+RANDOM_MAX+1) + RANDOM_MIN;
     }
 }
