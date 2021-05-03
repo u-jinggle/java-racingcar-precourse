b9c2643a34243f0f081e4d72c39f9d65cc6cfdd3
이유진 <ujin@deali.net>
2021-05-04 01:12:24 +0900

    test: 시도회수만큼 반복 완료 후 가장 이동이 많은 자동차 출력 기능 Test

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -45,5 +43,29 @@ class CarsTest {
         int playCount = 4;
 
         cars.play(playCount);
+    }
+
+    @Test
+    void getWinnerTest() {
+        int car1Move = 4;
+        int car2Move = 5;
+        int car3Move = 5;
+
+        for (int i = 0; i < car1Move; i++) {
+            car1.moveForward(9);
+        }
+        for (int i = 0; i < car2Move; i++) {
+            car2.moveForward(9);
+        }
+        for (int i = 0; i < car3Move; i++) {
+            car3.moveForward(9);
+        }
+        List<Car> carList = Arrays.asList(
+                car1, car2, car3
+        );
+        Cars cars = new Cars(carList);
+
+        String expectResult = "test2, test3가 최종 우승했습니다.";
+        assertThat(cars.getWinner()).isEqualTo(expectResult);
     }
 }
